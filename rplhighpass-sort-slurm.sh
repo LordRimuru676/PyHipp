f63821aee193da528c375b6175bc9c8a79c26e29
AWS ParallelCluster user <ec2-user@ip-10-0-3-109.ap-southeast-1.compute.internal>
2026-09-06 10:58:42 +0000

    Complete Lab 5 just-in-time and environment pool setup

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -9,6 +9,17 @@
 #SBATCH -o rplhps-slurm.%N.%j.out
 #SBATCH -e rplhps-slurm.%N.%j.err
 
+# Enable conda in this batch shell
+source /data/miniconda3/etc/profile.d/conda.sh
+
+# Borrow one cloned environment
+envarg=$(/data/src/PyHipp/envlist.py)
+
+echo "Using environment: $envarg"
+
+# Activate borrowed environment
+conda activate "$envarg"
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -17,3 +28,9 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+
+# Leave cloned environment
+conda deactivate
+
+# Return environment to pool
+/data/src/PyHipp/envlist.py "$envarg"
